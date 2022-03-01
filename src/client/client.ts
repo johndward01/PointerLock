b237157c4d750b834c8bfef870a9ccbd0b66da9f
johndward <johndward01@gmail.com>
2022-02-28 20:00:37 -0600

    stopping point 2: https://sbcode.net/threejs/animation-loop/

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
@@ -13,6 +14,9 @@ camera.position.z = 2
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
@@ -31,17 +35,18 @@ function onWindowResize() {
     render()
 }
 
-function animate() {
-    requestAnimationFrame(animate)
+// function animate() {
+//     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+//     cube.rotation.x += 0.01
+//     cube.rotation.y += 0.01
 
-    render()
-}
+//     render()
+// }
 
 function render() {
     renderer.render(scene, camera)
 }
 
-animate()
+//animate()
+render()
